08caf5ddc0d12856ae4b960d97dae2a63c57982f
ayush30197 <32996854+ayush30197@users.noreply.github.com>
2025-04-30 01:59:20 +0530

    Initial Commit

---
+++ src/main/java/com/github/ayush30197/spring_security_demo/HelloController.java
@@ -0,0 +1,19 @@
+package com.github.ayush30197.spring_security_demo;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("hello")
+    public String greet(HttpServletRequest request){
+        return "Hello, World " + request.getSession().getId();
+    }
+
+    @GetMapping("about")
+    public String about(HttpServletRequest request){
+        return "Trying to learn Spring Security " + request.getSession().getId();
+    }
+}
