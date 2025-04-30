a5cee520e67c3d3f1052f8572b81d7a6af150f12
ayush30197 <32996854+ayush30197@users.noreply.github.com>
2025-04-30 14:27:49 +0530

    Custom Security Config

---
+++ src/main/java/com/github/ayush30197/spring_security_demo/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.github.ayush30197.spring_security_demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+       /*
+        //Non Lambda code -- Imperative Style
+        Customizer<CsrfConfigurer<HttpSecurity>> csrfConfigurerCustomizer = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+            @Override
+            public void customize(CsrfConfigurer<HttpSecurity> httpSecurityCsrfConfigurer) {
+                httpSecurityCsrfConfigurer.disable();
+            }
+        };
+
+        http.csrf(csrfConfigurerCustomizer);
+
+        Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> authorizationManagerRequestMatcherRegistryCustomizer = new Customizer<AuthorizeHttpRequestsConfigurer<org.springframework.security.config.annotation.web.builders.HttpSecurity>.AuthorizationManagerRequestMatcherRegistry>() {
+            @Override
+            public void customize(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authorizationManagerRequestMatcherRegistry) {
+                authorizationManagerRequestMatcherRegistry.anyRequest().authenticated();
+            }
+        };
+        http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistryCustomizer);
+        */
+
+        //Lambda way of customizing security config
+       return http
+                .csrf(csrf -> csrf.disable())  //Disables the csrf
+                .authorizeHttpRequests(req -> req.anyRequest().authenticated()) //Authorize any http request
+                .formLogin(Customizer.withDefaults()) //Provides form login feature
+                .httpBasic(Customizer.withDefaults()) //Uses basic auth
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //Making application stateless
+               .build();
+    }
+}
